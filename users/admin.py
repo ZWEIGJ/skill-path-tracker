c204de36424b6f31867a95f560f85ff6e5b0aaf5
charlesgan <GanCharles522@gmail.com>
2026-04-10 17:36:41 +0800

    feat(users): 注册 CustomUser 到 Admin 后台，实现技能画像可视化配置

--- users/admin.py
+++ users/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
-# Register your models here.
+@admin.register(CustomUser)
+class CustomUserAdmin(UserAdmin):
+    # 在用户列表中显示的列，让你一眼看到所有人的技能水平
+    list_display = ('username', 'email', 'skill_level', 'is_staff')
+    
+    # 在修改用户详情页时，增加一个“技能画像”区块
+    fieldsets = UserAdmin.fieldsets + (
+        ("技能画像", {'fields': ('skill_level', 'skill_tags')}),
+    )
